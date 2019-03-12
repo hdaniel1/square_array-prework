1783224a0222e115c59b61ce21e6b643865fd8e9
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 16:46:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |num|
-    new_array.push(num ** 2)
-    return new_array# your code
+    new_array.push(num ** 2)our code
     end
+  return new_array
 end
